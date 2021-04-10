ade51200384e8cda8282ab425e0b3940ba929bce
susu <skyqnaqna@naver.com>
2021-04-11 03:15:17 +0900

    Update Queue

--- Queue/Queue.swift
+++ Queue/Queue.swift
@@ -49,3 +49,36 @@ extension QueueArray: CustomStringConvertible {
 
 // Doubly linked list implementation
 
+public class QueueLinkedList<T>: Queue {
+  private var list = DoublyLinkedList<T>()
+  public init() {}
+  
+  public func enqueue (_ element: T) -> Bool {
+    list.append(element)
+    return true
+  }
+  
+  public func dequeue() -> T? {
+    guard !list.isEmpty, let element = list.first else {
+      return nil
+    }
+    return list.remove(element)
+  }
+  
+  public var peek: T? {
+    return list.first?.value
+  }
+  
+  public var isEmpty: Bool {
+    return list.isEmpty
+  }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+  public var description: String {
+    return list.description
+  }
+}
+
+// Ring buffer implementation
+
