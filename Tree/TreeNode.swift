e53dcb77f5a43f2ded682d9db5467b5c9e66d5ac
susu <skyqnaqna@naver.com>
2021-04-12 23:56:46 +0900

    Add Tree

---
+++ Tree/TreeNode.swift
@@ -0,0 +1,28 @@
+//
+//  TreeNode.swift
+//  learnSwift
+//
+//  Created by Subin Kim on 2021/04/12.
+//
+
+public class TreeNode<T> {
+  public var value: T
+  public var children: [TreeNode] = []
+  
+  public init (_ value: T) {
+    self.value = value
+  }
+  
+  public func add (_ child: TreeNode) {
+    children.append(child)
+  }
+}
+
+extension TreeNode {
+  public func forEachDepthFirst (visit: (TreeNode) -> Void) {
+    visit(self)
+    children.forEach {
+      $0.forEachDepthFirst(visit: visit)
+    }
+  }
+}
